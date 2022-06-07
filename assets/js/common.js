d5d806209c70f8be2f7d6cdd74feb6f414499c53
kei037 <juneway@hanmail.net>
2022-06-07 17:46:49 +0900

    2022-06-07 study

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,11 @@
 $(function(){
-  
+  $(".mainmenu").each(function(){
+    $(this).mouseenter(function(){
+      $(this).find(".submenu-inner").removeClass("on");
+    })
+    .mouseleave(function(){
+      $(this).find(".submenu-inner").addClass("on");
+    })
+  })
+
 });
